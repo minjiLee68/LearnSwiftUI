a82d144761f9d953f021fd4ce484fad1b60c8a42
minjiLee68 <dlalswl2598@gmail.com>
2022-05-23 23:22:23 +0900

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by 이민지 on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
